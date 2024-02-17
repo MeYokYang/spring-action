eab0acaba0e93871fd92c84bc88269a5e9e9b9a2
meyok <2553774545@qq.com>
2024-02-18 05:55:19 +0800

    feat(TACO): 第一章内容代码实现。

---
+++ src/test/java/xyz/meyok/study/TacoCloudApplicationTests.java
@@ -0,0 +1,19 @@
+package xyz.meyok.study;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author meyokyang@163.com
+ * @date 17/02/2024 20:38
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TacoCloudApplicationTests {
+    @Test
+    public void contextLoads() {
+        
+    }
+}
